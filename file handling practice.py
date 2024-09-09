19fca43140658609d203875a49456535d0219b82
JoanDing08 <j.okia.ding08@gmail.com>
2024-09-09 15:55:39 +0100

    Update file handling practice.py

--- file handling practice.py
+++ file handling practice.py
@@ -5,3 +5,8 @@ with open("number file","w") as file:
     file.write("1,2,3,4,5,6,7,8,9,10")
 with open("number file") as file:
     print(file.read())
+
+with open("sum","w") as file:
+    file.write("55")
+with open("sum") as file:
+    print("the sum of these numbers is",file.read())
